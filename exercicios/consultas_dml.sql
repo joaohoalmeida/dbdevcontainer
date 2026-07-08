4e050b971055bdefe49cee518ba3de179aae4d65
joaohoalmeida <joao.heli@escolar.ifrn.edu.br>
2026-07-08 18:41:21 -0300

    commit inicial
    Add multiple entries to the pessoa table with INSERT commands.

---
+++ exercicios/consultas_dml.sql
@@ -0,0 +1,7 @@
+-- Cadastrar os elementos segundo as especificações abaixo (Comandos INSERT).
+
+INSERT INTO consultas.pessoa(nome, email, cpf, data_nasc, endereco, telefone)
+  VALUES ('Pedro I', 'pp@email.com', '002', '10-01-1479', 'R. Vasco', NULL),
+         ('Pedro II', 'ps@email.com', '003', '10-02-1516', 'R. Flamengo', '5501'),
+         ('D João VI', 'dj@email.com', '001', '01-12-1415', 'R. Portugal' NULL),
+         ('JJ Xavier', 'jj@email.com', '004', '12-11-1746', 'R. Minas', '5502');
