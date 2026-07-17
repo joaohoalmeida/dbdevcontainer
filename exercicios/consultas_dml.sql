b3bc96b1db50a0b31f3bee4fc293a2dbba80931b
joaohoalmeida <joao.heli@escolar.ifrn.edu.br>
2026-07-17 20:32:02 -0300

    correções

--- exercicios/consultas_dml.sql
+++ exercicios/consultas_dml.sql
@@ -2,10 +2,10 @@
 -- Cadastrar os elementos segundo as especificações abaixo (Comandos INSERT).
 
 INSERT INTO consultas.pessoa(nome, email, cpf, data_nasc, endereco, telefone)
-  VALUES ('Pedro I', 'pp@email.com', '002', '10-01-1479', 'R. Vasco', NULL),
-         ('Pedro II', 'ps@email.com', '003', '10-02-1516', 'R. Flamengo', '5501'),
-         ('D João VI', 'dj@email.com', '001', '01-12-1415', 'R. Portugal', NULL),
-         ('JJ Xavier', 'jj@email.com', '004', '12-11-1746', 'R. Minas', '5502');
+  VALUES ('Pedro I', 'pp@email.com', '002', '1479-01-10', 'R. Vasco', NULL),
+         ('Pedro II', 'ps@email.com', '003', '1516-02-10', 'R. Flamengo', '5501'),
+         ('D João VI', 'dj@email.com', '001', '1415-12-01', 'R. Portugal', NULL),
+         ('JJ Xavier', 'jj@email.com', '004', '1746-11-12', 'R. Minas', '5502');
 
 INSERT INTO consultas.paciente(cpf_pessoa, senha, plano_saude)
   VALUES('002', 'senha1', false),
@@ -26,16 +26,16 @@ INSERT INTO consultas.medico_especialidade(cpf_medico, id_especialidade)
         ('004', 3);
 
 INSERT INTO consultas.agendamento(cpf_paciente, cpf_medico, dh_consulta, dh_agendamento, valor_consulta)
-  VALUES ('002', '001', '14-04-1782 16:00:00', '14-03-1782 10:04:45', 80),
-         ('002', '004', '15-04-1782 10:00:00', '14-03-1782 10:04:45', 100),
-         ('002', '004', '17-05-1783 08:00:00', '10-05-1783 16:32:00', 100),
-         ('003', '001', '17-05-1783 08:30:00', '09-05-1783 09:05:56', 0);
+  VALUES ('002', '001', '1782-04-14 16:00:00', '1782-03-14 10:04:45', 80),
+         ('002', '004', '1782-04-15 10:00:00', '1782-03-14 10:04:45', 100),
+         ('002', '004', '1783-05-17 08:00:00', '1783-05-10 16:32:00', 100),
+         ('003', '001', '1783-05-17 08:30:00', '1783-05-09 09:05:56', 0);
 
 -- QUESTÃO 2
 -- Atualizar a data de nascimento de "D João VI", ele nasceu em "01-12-1416".
 
 UPDATE consultas.pessoa
-  SET data_nasc = '01-12-1416'
+  SET data_nasc = '1416-12-01'
   WHERE cpf = '001';
 
 -- Atualizar o telefone e e-mail de "D Pedro I" para "5503" e "pf@email.com" , respectivamente.
@@ -54,9 +54,9 @@ UPDATE consultas.pessoa
 -- As consultas do dia "17-05-1783" devem ser adiadas para o dia "19-05-1783", e o valor das consultas alteradas para R$ 150,00.
 
 UPDATE consultas.agendamento
-  SET dh_consulta = '19-05-1783 08:00:00',
+  SET dh_consulta = dh_consulta + INTERVAL '2 days',
       valor_consulta = 150
-  WHERE dh_consulta LIKE '17-05-1783%';
+  WHERE dh_consulta::date = '1783-05-17';
 
 -- O médico "JJ Xavier" não atenderá como "Cardiologista", somente como "Pediatra" e também "Ortopedista".
 
@@ -64,12 +64,11 @@ UPDATE consultas.medico_especialidade
   SET id_especialidade = 1
   WHERE cpf_medico = '004' and id_especialidade = 2;
 
-
 -- QUESTÃO 3
 -- O paciente "Pedro I" cancelou a consulta agendada para o dia "19-05-1783".
- 
+
 DELETE from consultas.agendamento 
-  WHERE dh_consulta LIKE '19-05-1783%' and cpf_paciente = '002';
+  WHERE dh_consulta::date = '1783-05-19' and cpf_paciente = '002';
 
 -- Remover os agendamentos com o médico "D João VI" com o custo de R$ 0,00. Utilize essas duas condições no comando DELETE.
 
