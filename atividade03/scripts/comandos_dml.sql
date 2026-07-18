c30dff669b2a99c785049c49f42f19f8a91e89b4
joaohoalmeida <joao.heli@escolar.ifrn.edu.br>
2026-07-18 09:35:55 -0300

    comandos dml

---
+++ atividade03/scripts/comandos_dml.sql
@@ -0,0 +1,15 @@
+-- Insert na tabela USUÁRIO
+INSERT INTO usuario 
+  VALUES ('11111111111', 'Ana Souza'),
+         ('22222222222', 'Bruno Lima'),
+         ('33333333333', 'Carla Mendes'),
+         ('44444444444', 'Diego Alves'),
+         ('55555555555', 'Elaine Costa');
+
+-- Insert na tabela VENDEDOR
+INSERT INTO vendedor
+  VALUES ('10000000000101', 'Leilões Norte Ltda', 'logo_norte.png'),
+         ('10000000000102', 'Casa de Leilões Sul', 'logo_sul.png'),
+         ('10000000000103', 'Antiguidades Brasil', 'logo_antiguidades.png'),
+         ('10000000000104', 'Leilões Premium', 'logo_premium.png'),
+         ('10000000000105', 'Bens & Cia Leilões', NULL);
