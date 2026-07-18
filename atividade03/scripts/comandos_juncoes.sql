af8a85e81fe547269d915e4a744665ba175a5d1e
joaohoalmeida <joao.heli@escolar.ifrn.edu.br>
2026-07-18 15:07:19 -0300

    finalização

--- atividade03/scripts/comandos_juncoes.sql
+++ atividade03/scripts/comandos_juncoes.sql
@@ -7,8 +7,14 @@ SELECT LOTE.nome, CATEGORIA.nome FROM LOTE
 -- Lista o nome do lote, leilão e o leiloeiro
 SELECT LOTE.nome, LEILAO.nome, LEILOEIRO.nome FROM LOTE
   INNER JOIN LEILAO ON LOTE.id_leilao = LEILAO.id_leilao
-  INNER JOIN LEILOEIRO ON LEILAO.CPF = LEILOEIRO.cpf
+  INNER JOIN LEILOEIRO ON LEILAO.CPF = LEILOEIRO.cpf;
 
--- D3. Um comando SELECT para exibir alguns dados dos registros das tabelas usando algum operadores de junção externa (ex. OUTER LEFT ou OUTER RIGHT JOIN).
+-- D3. SELECT com JUNÇÃO EXTERNA (LEFT OUTER JOIN)
+-- Lista todos os lotes e, quando existir, o valor do lance oferecido
+SELECT LOTE.nome, OFERECE_LANCE.lance, OFERECE_LANCE.cpf FROM LOTE
+  LEFT OUTER JOIN OFERECE_LANCE ON LOTE.id_lote = OFERECE_LANCE.id_lote;
 
 -- D4. Um comando SELECT para exibir alguns dados dos registros de tabelas usando o operador de junção qualificada FULL OUTER JOIN.
+-- Lista todas as categorias e todos os lotes.
+SELECT CATEGORIA.nome, LOTE.nome AS nome_lote FROM CATEGORIA
+  FULL OUTER JOIN LOTE ON CATEGORIA.id_categoria = LOTE.id_categoria;
